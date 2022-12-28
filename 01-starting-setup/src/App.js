249edaaa17bf907d42a44676f86e4a5ae2297207
Rezwan Atcha <rez_a@outlook.com>
2022-12-28 15:52:17 -0500

    Completed the assignment.

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,11 +1,36 @@
-import ExpenseItem from './components/ExpenseItem';
 import Expenses from './components/Expenses';
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.67,
+      date: new Date(2021, 7, 28),
+    },
+    {
+      id: 'e2',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e3',
+      title: 'Water Heater',
+      amount: 34.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'PC chair',
+      amount: 84.67,
+      date: new Date(2021, 4, 28),
+    },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <Expenses />
+      <Expenses items={expenses} />
     </div>
   );
 }
