c8e7a7283687be0aa24d915f9b593059b3fd28b5
Rezwan Atcha <rez_a@outlook.com>
2023-01-05 12:22:05 -0500

    Completed Section 3 of the course and moving to section 4.

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,4 +1,5 @@
-import Expenses from './components/Expenses';
+import React from 'react';
+import Expenses from './components/Expenses/Expenses';
 
 function App() {
   const expenses = [
@@ -17,16 +18,26 @@ function App() {
     {
       id: 'e3',
       title: 'Water Heater',
-      amount: 34.67,
+      amount: 34.17,
       date: new Date(2021, 2, 28),
     },
     {
       id: 'e4',
       title: 'PC chair',
-      amount: 84.67,
+      amount: 84.45,
       date: new Date(2021, 4, 28),
     },
   ];
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
+  // The following is the JSX version of the above commented code
+
   return (
     <div>
       <h2>Let's get started!</h2>
